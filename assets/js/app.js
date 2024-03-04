651602c1c3d6651c6e3984caca5eba4edc5f24da
Sandeep-508 <elsenothing321@gmail.com>
2024-03-04 11:58:24 +0530

    changed some margins in first section and added js in navitems

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,11 @@
 let btn = document.querySelector(".myBtn");
 let scrollPt = 200;
+let menu = document.querySelector(".menu");
+let lg_view = document.querySelector(".lg-view");
+
+menu.addEventListener("click", () => {
+    lg_view.classList.toggle("show");
+});
 
 window.addEventListener("scroll", () => {
     if (window.scrollY > scrollPt) {
@@ -11,4 +17,12 @@ window.addEventListener("scroll", () => {
 
 btn.addEventListener("click", () => {
     document.documentElement.scrollTop = 0;
+});
+
+let links = document.querySelectorAll(".nav-item");
+
+links.forEach(link => {
+    link.addEventListener("click", () => {
+        lg_view.classList.remove("show");
+    });
 });
