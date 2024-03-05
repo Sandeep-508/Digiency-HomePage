77d87493c2822ea9a63bd18290d9405a077a16bb
Sandeep-508 <elsenothing321@gmail.com>
2024-03-05 10:18:45 +0530

    completed projecet and corrected all the mistakes from sir

--- assets/js/app.js
+++ assets/js/app.js
@@ -2,9 +2,14 @@ let btn = document.querySelector(".myBtn");
 let scrollPt = 200;
 let menu = document.querySelector(".menu");
 let lg_view = document.querySelector(".lg-view");
-
+let body = document.body;
 menu.addEventListener("click", () => {
     lg_view.classList.toggle("show");
+    if (lg_view.classList.contains("show")) {
+        body.style.overflow = "hidden";
+    } else {
+        body.style.overflow = "auto";
+    }
 });
 
 window.addEventListener("scroll", () => {
@@ -24,5 +29,6 @@ let links = document.querySelectorAll(".nav-item");
 links.forEach(link => {
     link.addEventListener("click", () => {
         lg_view.classList.remove("show");
+        body.style.overflow = "auto";
     });
 });
